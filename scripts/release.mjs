e18b24aca5784d94f2f16f112d526a35a2d54ce1
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-08-26 22:00:37 +1000

    Fix release script

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -5,12 +5,12 @@ try {
   echo('Visit https://github.com/orhun/git-cliff#installation for installation instructions')
 }
 
-let tag = await question('What is the tag of the release?')
+let tag = await question('What is the tag of the release?\n')
 
-await $`git-cliff --tag ${tag} > CHANGELOG.md`
+await $`git-cliff --tag '${tag}' > CHANGELOG.md`
 await $`git add ./CHANGELOG.md`
 await $`git commit -m 'chore(release): ${tag}'`
-await $`git tag ${tag}`
+await $`git tag '${tag}'`
 
 echo('Finished')
 echo('Please push changes to upstream.')
